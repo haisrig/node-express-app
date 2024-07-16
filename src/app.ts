5acd6833634d96af9ccc34c0222242d8a4b946e9
srikanth Gunuputi <srikanth.gunuputi@gmail.com>
2024-07-16 19:23:00 +0530

    init commit

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello..!');
 });
-
+var aq = 5;
 app.listen(port, () => {
   return console.log(`Server is listening at http://localhost:${port}`);
 });
