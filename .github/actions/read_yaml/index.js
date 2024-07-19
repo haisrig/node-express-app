dce20e19a6a1489132c0bd2b7b9704ae7c7555f0
srikanth Gunuputi <srikanth.gunuputi@gmail.com>
2024-07-19 18:51:28 +0530

    new js custom action

--- .github/actions/read_yaml/index.js
+++ .github/actions/read_yaml/index.js
@@ -14,16 +14,14 @@ function run() {
     }
 
     const repoConfig = yaml.load(fs.readFileSync('config.yaml', 'utf8'));
-    console.log("Branch Name:", branchName);
     const branchConfig = repoConfig.branches.filter(branch => branch.type == branchType)[0];
-    console.log("Branch Config:", branchConfig);
+    core.log("Branch Config: ", branchConfig);
     core.setOutput("build_type", repoConfig.build_type);
     core.setOutput("static_code_analysis", repoConfig.static_code_analysis);
     core.setOutput("build", branchConfig.build.toString());
     core.setOutput("unit_test", branchConfig.unit_tests.toString());
     core.setOutput("code_analysis", branchConfig.code_analysis.toString());
     core.setOutput("store_artifact", branchConfig.store_artifact.toString());
-    console.log("unit_test:", branchConfig.unit_tests);
 
 
     // core.setOutput("build_type", branchConfig.build_type);
