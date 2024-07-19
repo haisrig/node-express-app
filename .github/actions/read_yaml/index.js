e558851b76c003142e434ff1ea4370627fe007f6
srikanth Gunuputi <srikanth.gunuputi@gmail.com>
2024-07-19 16:53:35 +0530

    new js custom action

--- .github/actions/read_yaml/index.js
+++ .github/actions/read_yaml/index.js
@@ -21,6 +21,10 @@ function run() {
     console.log("Branch Config:", branchConfig);
     core.setOutput("build_type", repoConfig.build_type);
     core.setOutput("static_code_analysis", repoConfig.static_code_analysis);
+    core.setOutput("build", branchConfig.build);
+    core.setOutput("unit_test", branchConfig.unit_test);
+    core.setOutput("code_analysis", branchConfig.code_analysis);
+    core.setOutput("store_artifact", branchConfig.store_artifact);
 
 
     // core.setOutput("build_type", branchConfig.build_type);
